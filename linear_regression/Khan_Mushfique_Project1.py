d089d2dae53cfbffe4650642baa9da3910e79b6c
Mushfiquehk <mushfiquehasankhan@gmail.com>
2022-01-30 21:37:03 -0600

    Applied gradient descent algorithm to linear regression

--- linear_regression/Khan_Mushfique_Project1.py
+++ linear_regression/Khan_Mushfique_Project1.py
@@ -1,7 +1,22 @@
+""" Dependencies: python 3.9.7
+                  matplotlib 3.5.0
+                  numpy 1.21.2
+                  openpyxl 0.46
+                  pandas 1.3.5 """
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def J(weight, design, target):
+    """ Input: W: weight matrix 1,1   (D+1X1)
+               X: design matrix 406,2 (NXD+1)
+               t: target vector 406,1 (NX1)
+        Output: gradient with respect to W  1,2 (1XD+1) 
+        gradient of J = W^T.X^T.X - t^T.X 
+        (with respect to W)"""
+    return weight.T.dot(design.T.dot(design)) - target.T.dot(design)
+
+# Enter filename here
 filepath = "proj1Dataset.xlsx"
 df  = pd.read_excel(filepath)
 
@@ -14,19 +29,42 @@ X0 = np.ones((rows,1))
 # create design matrix of shape N x D+1
 design_matrix = np.hstack((features, X0))
 
-pseudoinverse = np.linalg.pinv(design_matrix)
-
 t = df['Horsepower']
 t = pd.DataFrame(t, columns=["Horsepower"])
 t = t.fillna(0)
 
-W = np.matmul(pseudoinverse, t)
+# Closed form solution
+
+W = (np.linalg.pinv(design_matrix))@t
 Y = design_matrix@W
 
-plt.scatter(df['Weight'], df['Horsepower'],c='red',marker='x')
-plt.xlabel("Weight")
-plt.ylabel("Horsepower")
-plt.plot(df['Weight'], Y, c='b',label='Closed Form')
-plt.legend(loc='upper right')
-plt.title("Matlab's 'carbig' dataset")
-plt.savefig("Closed Form.jpg")
+y_cf_plot = plt.scatter(df['Weight'], df['Horsepower'],c='red',marker='x')
+y_cf_plot = plt.xlabel("Weight")
+y_cf_plot = plt.ylabel("Horsepower")
+y_cf_plot = plt.plot(df['Weight'], Y, c='b',label='Closed Form')
+y_cf_plot = plt.legend(loc='upper right')
+y_cf_plot = plt.title("Matlab's 'carbig' dataset")
+y_cf_plot = plt.savefig("Closed Form.jpg")
+
+# Gradient Descent Section
+
+np.random.seed(33)
+learning_rate = 0.0000000001
+num_iterations = 5000
+
+W0 = np.random.randn(2,1)
+W_GD = W0
+weightX = pd.DataFrame(design_matrix, columns=['Weight', 'W0'])
+
+for i in range(num_iterations):
+    W_temp = W_GD - learning_rate*(J(W_GD, weightX, t).T)
+    W_GD = W_temp
+
+Y_GD = design_matrix@W_GD
+y_gd_plot = plt.scatter(df['Weight'], df['Horsepower'],c='red',marker='x')
+y_gd_plot = plt.xlabel("Weight")
+y_gd_plot = plt.ylabel("Horsepower")
+y_gd_plot = plt.plot(df['Weight'], Y_GD, c='g',label='Gradient Descent')
+y_gd_plot = plt.legend(loc='upper right')
+y_gd_plot = plt.title("Matlab's 'carbig' dataset")
+y_gd_plot = plt.savefig("Gradient Descent.jpg")
